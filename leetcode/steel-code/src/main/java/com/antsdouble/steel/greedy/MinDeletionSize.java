db7ac8a82eabbda70c93e1c81138a9c237e41aac
liyy <liyangyang@everisetech.com>
2019-09-17 09:31:54 +0800

    deal simple code

--- leetcode/steel-code/src/main/java/com/antsdouble/steel/greedy/MinDeletionSize.java
+++ leetcode/steel-code/src/main/java/com/antsdouble/steel/greedy/MinDeletionSize.java
@@ -16,6 +16,8 @@ public class MinDeletionSize {
         solution(testArray);
     }
 
+    //第一版
+
     public static int solution(String[] A) {
         int result = 0;
         List<List<Character>> listChar = new ArrayList<>(100);
@@ -40,6 +42,26 @@ public class MinDeletionSize {
             }
         }
 
+        return result;
+    }
+
+    /***
+     * 功能描述 //第二版
+     * @author lyy
+     * @date 2019/9/17
+     * @param [A]
+     * @return int
+     */
+    public static int solution2(String[] A) {
+        int result = 0;
+        for (int num = 0; num < A[0].length(); num++) {
+            for (int j = 0; j < A.length - 1; j++) {
+                if (A[j].charAt(num) > A[j + 1].charAt(num)) {
+                    result++;
+                    break;
+                }
+            }
+        }
         return result;
     }
 }
