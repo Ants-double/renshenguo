1b990e910ea0475a90b81baad3255d1211ae3f6b
liyy <ants_double@yeah.net>
2019-10-10 09:17:39 +0800

    map demo init

---
+++ DemoMap/src/main/java/com/antsdouble/demomap/MapApplication.java
@@ -0,0 +1,13 @@
+package com.antsdouble.demomap;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2019/10/10
+ */
+public class MapApplication {
+
+    public static void main(String[] args) {
+        System.out.println("map");
+    }
+}
---
+++ DemoMap/src/test/java/com/antsdouble/demomap/MapTest.java
@@ -0,0 +1,9 @@
+package com.antsdouble.demomap;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2019/10/10
+ */
+public class MapTest {
+}
